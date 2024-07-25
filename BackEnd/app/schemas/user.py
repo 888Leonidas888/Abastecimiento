7db74f7a3b948f29b7c71aeb173cd7e3f36d14b1
Planisph3r3 <ennzopz@gmail.com>
2024-07-25 17:37:18 -0500

    chore(user.schema): Merge from development branch

--- BackEnd/app/schemas/user.py
+++ BackEnd/app/schemas/user.py
@@ -1,23 +1,21 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
 class UserBase(BaseModel):
     dni: str
     user: str
     name_user: str
-    last_name: str
+    last_name_user: str
     permission: str
 
 
 class UserCreate(UserBase):
     password: str
 
-
 class UserUpdate(BaseModel):
     password: Optional[str] = None
     name_user: Optional[str] = None
-    last_name: Optional[str] = None
+    last_name_user: Optional[str] = None
     permission: Optional[str] = None
 
 class User(UserBase):
