0e341817bb3a8136ebfdc63307eb5e73c328acca
Planisph3r3 <ennzopz@gmail.com>
2024-07-26 20:41:37 -0500

    feat!(token_mngr): Added a token auth for certain endpoints

--- BackEnd/app/main.py
+++ BackEnd/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.users import router as router_users
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from app.token_manager import *
 
 app = FastAPI()
 
@@ -12,9 +15,23 @@ app.add_middleware(
     allow_headers=["*"],  # Permitir todos los encabezados
 )
 
-app.include_router(router_users, prefix='/api/v1', tags=['users'])
+app.include_router(router_users, prefix='/api/v1', tags=['users'], dependencies=[Depends(get_admin_user)])
 
 
 @app.get('/')
-def home():
-    return {'message': 'Hello world!'}
+def home(token: str = Depends(oauth_schema)):
+    return {'message': token}
+
+@app.post('/token')
+def login(form_data:OAuth2PasswordRequestForm = Depends()):
+    user = auth_user('users', form_data.username, form_data.password)
+    # Check if user is correct
+    print(user)
+    access_token_expire = timedelta(hours=8)
+    access_token_JWT = create_token({"sub": user['user']}, access_token_expire)
+    # Data from form
+    print(form_data.username, form_data.password)
+    return {
+        "access_token": access_token_JWT,
+        "token_time": "bearer"
+    }
