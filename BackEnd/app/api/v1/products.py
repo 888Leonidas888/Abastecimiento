a1d05d6b0b51c716ea683db7df08d0ea950a936b
Jhony <Jhonny14_1@hotmail.com>
2024-07-30 00:32:27 -0500

    fix(endpoint-products): corrige errores. - implementa transacciones para corregir errores. - implementa schema para entregar respuestas. - implementa funciones auxiliares para las transacciones. - error en inserción de nuevos productos por lote. - error en elimación de productos por lote.

--- BackEnd/app/api/v1/products.py
+++ BackEnd/app/api/v1/products.py
@@ -2,17 +2,21 @@ from fastapi import APIRouter, HTTPException
 from app.schemas.product import ProductList
 from app.schemas.batch import Batch
 from app.schemas.message import Message
-from app.utils.helpers import generate_batch, generate_id
+from app.utils.helpers import generate_batch, create_query_for_insert
+from app.utils.helpers import create_query_for_delete, generate_id
 from datetime import date
 from app.db.DB_Manager import Database
 
 router = APIRouter()
+table_products = 'products'
+table_batches = 'batches'
 
 
 @router.post('/products', response_model=Batch)
 def created_products(products: ProductList):
     count: int = 0
-    table_products: str = 'products'
+    total: int = 0
+
     batch_id: str = generate_batch()
 
     new_batch: dict = {
@@ -25,7 +29,11 @@ def created_products(products: ProductList):
     db.connect()
 
     try:
-        db.create('batches', **new_batch)
+        db.connection.start_transaction()
+        q_batch, v_batch = create_query_for_insert(table_batches, **new_batch)
+        db.cursor.execute(q_batch, v_batch)
+
+        total = len(products.products)
 
         for product in products.products:
             new_product = product.model_dump()
@@ -34,15 +42,19 @@ def created_products(products: ProductList):
             new_product['status'] = 'pendiente'
             new_product['created_at'] = date.today()
             new_product['updated_at'] = date.today()
-
-            db.create(table_products, **new_product)
+            q_product, v_product = create_query_for_insert(
+                table_products, **new_product)
+            db.cursor.execute(q_product, v_product)
             count += 1
+        db.connection.commit()
+        return {'batch': batch_id, 'total_products': f'{count} to {total}'}
     except Exception as e:
+        if db.connection:
+            db.connection.rollback()
         raise HTTPException(
-            status_code=500, detail=f'Error inserting products: {e}')
+            status_code=500, detail=f'Error {e}')
     finally:
         db.disconnect()
-    return {'batch': batch_id, 'total_products': count}
 
 
 @router.delete('/products/{batch_id}', response_model=Message)
@@ -51,20 +63,21 @@ def products_delete(batch_id: str):
     db = Database()
     db.connect()
     try:
-        result = db.read('batches', id=batch_id)
-
-        if len(result) == 0:
-            raise HTTPException(status_code=404,
-                                detail=f'{batch_id} not found')
-
+        db.connection.start_transaction()
         'Delete products for batches'
-        db.delete('products', batch_id=batch_id)
+        q_products, v_products = create_query_for_delete(
+            table_products, batch_id=batch_id)
+        db.cursor.execute(q_products, v_products)
 
         'Delete bacth id'
-        db.delete('batches', id=batch_id)
-
+        q_batches, v_batches = create_query_for_delete(
+            table_batches, id=batch_id)
+        db.cursor.execute(q_batches, v_batches)
+        db.connection.commit()
         return {'message': 'Batch and associated products deleted successfully.'}
     except Exception as e:
+        if db.connection:
+            db.connection.rollback()
         raise HTTPException(
             status_code=500,
             detail=f'Error deleting batch and products: {e}'
