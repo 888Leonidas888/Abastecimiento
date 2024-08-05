4c84b69171aaafb92e2da0b98b7c1eb10df1c39a
echanjan <113923012+echanjan@users.noreply.github.com>
2024-08-04 23:44:36 -0500

    feat: se corrigen funciones y se añade funcion deleteuser

--- FrontEnd/public/js/users.js
+++ FrontEnd/public/js/users.js
@@ -1,13 +1,16 @@
-const buttonAdd = document.getElementById("addUser");
-const modalAddUser = document.getElementById("modalAddUser");
-const modalUpdateUser = document.getElementById("modalUpdateUser");
-const closeAddUser = document.getElementById("closeAddUser");
-const closeUpdateUser = document.getElementById("closeUpdateUser");
-const overlay = document.getElementById("overlay");
-const userCreate = document.getElementById("userCreate");
-const userUpdate = document.getElementById('userUpdate')
-
 document.addEventListener("DOMContentLoaded", () => {
+  const buttonAdd = document.getElementById("addUser");
+  const modalAddUser = document.getElementById("modalAddUser");
+  const modalUpdateUser = document.getElementById("modalUpdateUser");
+  const modalDeleteUser = document.getElementById("modalDeleteUser"); 
+  const closeAddUser = document.getElementById("closeAddUser");
+  const closeUpdateUser = document.getElementById("closeUpdateUser");
+  const boxNoDelete = document.querySelector(".boxNoDelete");
+  const boxYesDelete = document.querySelector(".boxYesDelete");
+  const overlay = document.getElementById("overlay");
+  const userCreate = document.getElementById("userCreate");
+  const userUpdate = document.getElementById("userUpdate");
+
   tableUsers();
 
   buttonAdd.addEventListener("click", () => {
@@ -27,36 +30,54 @@ document.addEventListener("DOMContentLoaded", () => {
     createUser();
   });
 
-  userUpdate.addEventListener('click', async (event) => {
+  userUpdate.addEventListener("click", async (event) => {
     event.preventDefault();
-
-    const permission = document.getElementById('permissionUUpdate').value;
-    const name_user = document.getElementById('updateNombres').value;
-    const last_name_user = document.getElementById('updateApellidos').value;
-    const password = document.getElementById('updateConreseña').value;
-
-    const response = await fetch(`/updateUser/${selectedDNI}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        password: password,
-        name_user: name_user,
-        last_name_user: last_name_user,
-        permission: permission
-      })
-    });
-
-    if (response.ok) {
-      const result = await response.json();
-      console.log('Usuario actualizado:', result);
-      tableUsers();
-      closeModal(modalUpdateUser, overlay);
-    } else {
-      console.error('Error al actualizar el usuario');
-    }
+    updateUser();
   });
+
+  boxNoDelete.addEventListener("click", () => {
+    closeModal(modalDeleteUser, overlay);
+  });
+
+  boxYesDelete.addEventListener("click", () => {
+    deleteUser();
+    closeModal(modalDeleteUser, overlay);
+  })
+
+  document.getElementById('hola').addEventListener('click', () => {
+    activateBarSuccess();
+  });
+
+  // userUpdate.addEventListener('click', async (event) => {
+  //   event.preventDefault();
+
+  //   const permission = document.getElementById('permissionUUpdate').value;
+  //   const name_user = document.getElementById('updateNombres').value;
+  //   const last_name_user = document.getElementById('updateApellidos').value;
+  //   const password = document.getElementById('updateConreseña').value;
+
+  //   const response = await fetch(`/updateUser/${selectedDNI}`, {
+  //     method: 'PUT',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       password: password,
+  //       name_user: name_user,
+  //       last_name_user: last_name_user,
+  //       permission: permission
+  //     })
+  //   });
+
+  //   if (response.ok) {
+  //     const result = await response.json();
+  //     console.log('Usuario actualizado:', result);
+  //     tableUsers();
+  //     closeModal(modalUpdateUser, overlay);
+  //   } else {
+  //     console.error('Error al actualizar el usuario');
+  //   }
+  // });
 });
 
 let selectedDNI;
@@ -88,26 +109,30 @@ function tableUsers() {
 
         tr.innerHTML = `
                     <td><span class="material-symbols-rounded editUser">edit</span></td>
-                    <td class="userDNI">${registro.dni}</td>
+                    <td class="userDNI disguise">${registro.dni}</td>
                     <td>${registro.user}</td>
-                    <td>${registro.name_user}</td>
-                    <td>${registro.last_name_user}</td>
+                    <td class="disguise">${registro.name_user}</td>
+                    <td class="disguise">${registro.last_name_user}</td>
                     <td>${registro.permission}</td>
                     <td><span class="material-symbols-rounded deleteUser">delete</span></td>
                   `;
 
         tr.querySelector(".editUser").addEventListener("click", () => {
-          selectedDNI = tr.querySelector('.userDNI').textContent;
+          selectedDNI = tr.querySelector(".userDNI").textContent;
           showModal(modalUpdateUser, overlay);
 
-          document.getElementById('updateConreseña').value = '';
-          document.getElementById('updateNombres').value = registro.name_user;
-          document.getElementById('updateApellidos').value = registro.last_name_user;
-          document.getElementById('permissionUUpdate').value = registro.permission;
+          document.getElementById("updateConreseña").value = "";
+          document.getElementById("updateNombres").value = registro.name_user;
+          document.getElementById("updateApellidos").value =
+            registro.last_name_user;
+          document.getElementById("permissionUUpdate").value =
+            registro.permission;
         });
 
         tr.querySelector(".deleteUser").addEventListener("click", () => {
-          // Aquí podrías implementar la funcionalidad para eliminar el usuario
+          selectedDNI = tr.querySelector(".userDNI").textContent;
+          showModal(modalDeleteUser, overlay);
+          console.log('click', selectedDNI)
         });
 
         tbody.appendChild(tr);
@@ -147,6 +172,7 @@ function createUser() {
     .then((data) => {
       console.log("Usuario creado:", data);
       tableUsers();
+      activateBarSuccess();
       clearForm();
       closeModal(modalAddUser, overlay);
     })
@@ -155,11 +181,70 @@ function createUser() {
     });
 }
 
+async function updateUser() {
+  const permission = document.getElementById("permissionUUpdate").value;
+  const name_user = document.getElementById("updateNombres").value;
+  const last_name_user = document.getElementById("updateApellidos").value;
+  const password = document.getElementById("updateConreseña").value;
+
+  const response = await fetch(`/updateUser/${selectedDNI}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      password: password,
+      name_user: name_user,
+      last_name_user: last_name_user,
+      permission: permission,
+    }),
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    console.log("Usuario actualizado:", result);
+    tableUsers();
+    closeModal(modalUpdateUser, overlay);
+    console.log(result.message);
+  } else {
+    console.error("Error al actualizar el usuario");
+  }
+}
+
+async function deleteUser() {
+  const response = await fetch(`/updateUser/${selectedDNI}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    const result = await response.json();
+    console.log("Usuario eliminado:", result);
+    // alert("Usuario eliminado");
+    activateBarSuccess();
+    tableUsers();
+    console.log(result.message);
+  } else {
+    console.error("Error al actualizar el usuario");
+  }
+}
+
 function clearForm() {
-  document.getElementById('dni').value = '';
-  document.getElementById('nombreUsuario').value = '';
-  document.getElementById('contraseña').value = '';
-  document.getElementById('nombre').value = '';
-  document.getElementById('apellido').value = '';
-  document.getElementById('permissionUAdd').selectedIndex = 0;
+  document.getElementById("dni").value = "";
+  document.getElementById("nombreUsuario").value = "";
+  document.getElementById("contraseña").value = "";
+  document.getElementById("nombre").value = "";
+  document.getElementById("apellido").value = "";
+  document.getElementById("permissionUAdd").selectedIndex = 0;
+}
+
+function activateBarSuccess() {
+  const barSuccess = document.querySelector('.barSuccess');
+  if (barSuccess) {
+    barSuccess.classList.add('activate');
+    setTimeout(() => {
+      barSuccess.classList.remove('activate');
+    }, 2000); // 2000 ms = 2 seconds
+  }
 }
