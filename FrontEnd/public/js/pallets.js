04203c8530646058fc863ac8591f4e33f73f6d33
Jhony <Jhonny14_1@hotmail.com>
2024-08-16 19:33:26 -0500

    refac(fronted): actualizacódigo y agrega docs.

--- FrontEnd/public/js/pallets.js
+++ FrontEnd/public/js/pallets.js
@@ -37,7 +37,7 @@ function showUbicationPallet(ubication, qr) {
       <span class="material-symbols-rounded"> check_circle </span>
     </div>
   </div>
-    `;
+  `;
 
   boxPallets.appendChild(destinationProduct);
 }
@@ -69,19 +69,19 @@ function closeModal(modal, overlay) {
  * @param {string} id_user - The ID of the user making the update.
  * @returns {Promise} - A promise that resolves with the API response or rejects with an error.
  */
-function putPallets(qr_pallet, status, id_user) {
+function putPallets(qr_pallet, status, userSession) {
   return new Promise((resolve, reject) => {
     const apiUrl = 'http://localhost:8000/api/v1/pallets';
     const queryParams = {
-      qr_pallet,
+      qr_pallet: qr_pallet,
     };
     const headers = {
-      Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+      Authorization: `Bearer ${userSession.token}`,
       'Content-Type': 'application/json',
     };
     const body = {
-      status,
-      id_user,
+      status: status,
+      id_user: userSession.idUser,
     };
     axios
       .put(apiUrl, body, { params: queryParams, headers })
@@ -98,13 +98,13 @@ function putPallets(qr_pallet, status, id_user) {
  * Retrieves a list of pallets with a 'pendiente' (pending) status from the API.
  * @returns {Promise} - A promise that resolves with the API response or rejects with an error.
  */
-function getPallets() {
+function getPallets(userSession) {
   return new Promise((resolve, reject) => {
     const apiUrl = 'http://localhost:8000/api/v1/pallets?status=pendiente';
     axios
       .get(apiUrl, {
         headers: {
-          Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+          Authorization: `Bearer ${userSession.token}`,
           'Content-Type': 'application/json',
         },
       })
@@ -129,12 +129,11 @@ function getPallets() {
  * @param {HTMLElement} fields.freshness - The element to show the pallet's freshness.
  * @returns {void}
  */
-function showDetailPallets(fields) {
-  getPallets()
+function showDetailPallets(fields, userSession, processPallet) {
+  getPallets(userSession)
     .then((response) => {
       const pallets = response.data.pallets;
       const countPallets = pallets.length;
-      const idUser = sessionStorage.getItem('id_user');
 
       if (countPallets > 0) {
         fields.qr.value = pallets[0].qr_pallet;
@@ -145,55 +144,73 @@ function showDetailPallets(fields) {
         fields.dueDate.innerText = pallets[0].due_date;
         fields.freshness.innerText = pallets[0].freshness;
 
-        putPallets(pallets[0].qr_pallet, 'proceso', idUser)
-          .then((r) => {
-            // console.log(r);
-          })
+        putPallets(pallets[0].qr_pallet, processPallet.process, userSession)
+          .then((r) => {})
           .catch((err) => {
-            // console.log(err.message);
             alert(err.message);
           });
-      } else {
-        alert('No hay mas tareas pendientes.');
       }
     })
     .catch((err) => {
-      alert(err.message);
+      alert('No hay mas tareas pendientes.');
+      window.location.href = '/menu';
     });
 }
 
-function palletFound(qr_pallet, qr_current, status, id_user) {
-  const idUser = sessionStorage.getItem('id_user');
-  if (qr_pallet.toUpperCase() === qr_current.toUpperCase()) {
-    putPallets(qr_pallet, status, idUser)
-      .then((r) => {
-        hidePendingProduct(product);
-        showUbicationPallet(destination.value);
-      })
-      .catch((err) => {
-        alert(err.message);
-        // console.log(err.message);
-      });
-  }
-}
-
-function palletNotFound(qr_pallet, status, id_user) {
-  putPallets(qr_pallet, status, id_user)
+/**
+ * Updates the status of a pallet in the system and redirects the user to the main menu upon success.
+ *
+ * This function sends a request to update the status of a specific pallet using its QR code.
+ * If the request is successful, the user is redirected to the main menu.
+ * In case of an error, an alert is shown with the error message.
+ *
+ * @param {string} qr_pallet - The QR code of the pallet to update.
+ * @param {string} status - The new status to set for the pallet.
+ * @param {Object} userSession - The current user session object.
+ * @param {string} userSession.token - The authorization token of the user.
+ * @param {string} userSession.idUser - The ID of the user making the update.
+ *
+ * @returns {void}
+ */
+function palletUpdateState(qr_pallet, status, userSession) {
+  putPallets(qr_pallet, status, userSession)
     .then((r) => {
-      // console.log(r);
       window.location.href = '/menu';
     })
     .catch((err) => {
       alert(err.message);
-      // console.log(err.message);
     });
 }
 
-function verifyQRPallet(qr, qr_current) {
+/**
+ * Verifies if the current QR code matches the expected QR code and updates the pallet status accordingly.
+ *
+ * This function compares the provided QR code with the expected QR code. If they match, it hides the pending product element,
+ * displays the pallet's location details, and allows the user to confirm the location. If the locations match, the pallet status
+ * is updated to "completed". If the QR codes do not match, a modal with an overlay is shown.
+ *
+ * @param {string} qr - The expected QR code of the pallet.
+ * @param {string} qr_current - The current QR code input by the user.
+ * @param {Object} processPallet - An object containing the possible states for the pallet process.
+ * @param {string} processPallet.completed - The status value indicating the pallet process is completed.
+ * @param {Object} userSession - The current user session object.
+ * @param {string} userSession.token - The authorization token of the user.
+ * @param {string} userSession.idUser - The ID of the user making the update.
+ * @param {HTMLElement} modal - The modal element to display if the QR codes do not match.
+ * @param {HTMLElement} overlay - The overlay element to display if the QR codes do not match.
+ *
+ * @returns {void}
+ */
+function verifyQRPallet(
+  qr,
+  qr_current,
+  processPallet,
+  userSession,
+  modal,
+  overlay
+) {
   const product = document.getElementById('get-pending-product');
   const destination = document.getElementById('pallet-destination');
-  const modal = document.getElementById('modalExito');
-  const overlay = document.getElementById('overlay');
 
   if (qr_current !== '' && qr_current.toUpperCase() === qr.toUpperCase()) {
     hidePendingProduct(product);
@@ -203,11 +220,10 @@ function verifyQRPallet(qr, qr_current) {
     const ubication = document.getElementById('confirm-ubication').innerText;
 
     btnOK.addEventListener('click', () => {
-      const idUser = sessionStorage.getItem('id_user');
       const ubicationCurrent =
         document.getElementById('current-ubication').value;
-      if (ubication === ubicationCurrent) {
-        palletNotFound(qr_current, 'completado', idUser);
+      if (ubication.toUpperCase() === ubicationCurrent.toUpperCase()) {
+        palletUpdateState(qr_current, processPallet.completed, userSession);
       } else {
         alert('Ubicación incorrecta.');
       }
@@ -216,60 +232,82 @@ function verifyQRPallet(qr, qr_current) {
     showModal(modal, overlay);
   }
 }
-function showUserInfo() {
+
+/**
+ * Displays the user's profile information in the UI.
+ *
+ * This function updates the UI with the user's name and permission level
+ * by setting the inner HTML of specific elements with the corresponding values
+ * from the `userSession` object.
+ *
+ * @param {Object} userSession - The current user session object.
+ * @param {string} userSession.userName - The name of the user to display.
+ * @param {string} userSession.permission - The permission level of the user to display.
+ *
+ * @returns {void}
+ */
+function showUserInfo(userSession) {
   const nameProfile = document.querySelector('.nameProfile');
   const modeProfile = document.querySelector('.modeProfile');
-  const userName = sessionStorage.getItem('user');
-  const permission = sessionStorage.getItem('permission');
 
-  nameProfile.innerHTML = userName;
-  modeProfile.innerHTML = permission;
+  nameProfile.innerHTML = userSession.userName;
+  modeProfile.innerHTML = userSession.permission;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const idUser = sessionStorage.getItem('id_user');
-  const sku = document.getElementById('sku-pallet');
-  const origin = document.getElementById('ubication-pallet');
-  const description = document.getElementById('description-pallet');
-  const freshness = document.getElementById('freshness-pallet');
-  const dueDate = document.getElementById('due-date-pallet');
-  const qr = document.getElementById('qr-pallet-verify');
-  const destination = document.getElementById('pallet-destination');
-  const btnVerify = document.getElementById('btn-verify');
-  const btnFail = document.getElementById('btn-fail');
-  const qr_current = document.getElementById('qr-pallet');
-  const hiddenModal = document.querySelector('.close');
-  const process = {
+  const d = document;
+  const processPallet = {
     error: 'error',
+    process: 'proceso',
     completed: 'completado',
   };
 
-  const fields = {
-    sku,
-    origin,
-    description,
-    freshness,
-    dueDate,
-    qr,
-    destination,
+  const userSession = {
+    userName: sessionStorage.getItem('user'),
+    permission: sessionStorage.getItem('permission'),
+    idUser: sessionStorage.getItem('id_user'),
+    token: sessionStorage.getItem('token'),
   };
 
-  showDetailPallets(fields);
-  showUserInfo();
+  const btnVerify = d.getElementById('btn-verify');
+  const btnFail = d.getElementById('btn-fail');
+  const qr_current = d.getElementById('qr-pallet');
+  const hiddenModal = d.querySelector('.close');
+
+  const fields = {
+    sku: d.getElementById('sku-pallet'),
+    origin: d.getElementById('ubication-pallet'),
+    description: d.getElementById('description-pallet'),
+    freshness: d.getElementById('freshness-pallet'),
+    dueDate: d.getElementById('due-date-pallet'),
+    qr: d.getElementById('qr-pallet-verify'),
+    destination: d.getElementById('pallet-destination'),
+  };
+
+  const modal = d.getElementById('modalExito');
+  const overlay = d.getElementById('overlay');
+
+  showDetailPallets(fields, userSession, processPallet);
+  showUserInfo(userSession);
 
   hiddenModal.addEventListener('click', () => {
-    const modal = document.getElementById('modalExito');
-    const overlay = document.getElementById('overlay');
     closeModal(modal, overlay);
     qr_current.value = '';
     qr_current.focus();
   });
 
   btnVerify.addEventListener('click', () => {
-    verifyQRPallet(qr.value, qr_current.value);
+    verifyQRPallet(
+      fields.qr.value,
+      qr_current.value,
+      processPallet,
+      userSession,
+      modal,
+      overlay
+    );
   });
 
   btnFail.addEventListener('click', () => {
-    palletNotFound(qr.value, process.error, idUser);
+    palletUpdateState(fields.qr.value, processPallet.error, userSession);
   });
 });
